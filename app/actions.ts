e6815a8a26458191dd55597c048b0e5b21bfc705
Andrei Kondrashov <kondraschov.andrew1999@gmail.com>
2025-04-18 15:10:36 -0400

    add fetchSubFolders server action

--- app/actions.ts
+++ app/actions.ts
@@ -194,3 +194,55 @@ export async function createFolder(formData: FormData) {
     content: folder_in_folder,
   };
 }
+
+export interface Folder {
+  id: string;
+}
+
+export interface FolderInFolder {
+  folder_id: string;
+  folders: Folder;
+  folder_name: string;
+}
+
+export async function fetchSubFolders(
+  folder_name: string,
+  parent_id?: "00000000-0000-0000-0000-000000000000"
+): Promise<{
+  success: boolean;
+  message: string;
+  content: FolderInFolder[];
+}> {
+  const supabase = await createClient();
+  const {
+    data: { user_id },
+    error: err_fetching_user,
+  } = await supabase.rpc("fetch_current_user_id");
+  if (err_fetching_user) {
+    return {
+      success: false,
+      message: err_fetching_user.message,
+      content: [],
+    };
+  }
+
+  const { data: folders, error } = await supabase
+    .from("folder_in_folder")
+    .select("*")
+    .eq("user_id", user_id)
+    .eq("folder_name", folder_name)
+    .eq("parent_id", parent_id);
+
+  if (error) {
+    return {
+      success: false,
+      content: [],
+      message: error.message,
+    };
+  }
+  return {
+    success: true,
+    message: "",
+    content: folders as FolderInFolder[],
+  };
+}
