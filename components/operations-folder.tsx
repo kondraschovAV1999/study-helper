fc8a08a992a0db186416195e0e2bbbbd7c859656
Andrei Kondrashov <kondraschov.andrew1999@gmail.com>
2025-04-25 20:02:28 -0400

    extracted component for operations on folders

---
+++ components/operations-folder.tsx
@@ -0,0 +1,58 @@
+"use client";
+import { Trash2 } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { DialogOption } from "./left-sidebar";
+import { JSX } from "react";
+
+export type Folder = {
+  id: string;
+  name: string;
+};
+
+export default function OperationsFolder({
+  folder,
+  setSelectedFolder,
+  setActiveDialog,
+  setIsDialogOpen,
+  Trigger,
+}: {
+  folder: Folder;
+  setSelectedFolder: (folder: Folder) => void;
+  setActiveDialog: (option: DialogOption) => void;
+  setIsDialogOpen: (open: boolean) => void;
+  Trigger: () => JSX.Element;
+}) {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>{Trigger()}</DropdownMenuTrigger>
+      <DropdownMenuContent className="z-[100] w-fit p-0">
+        <DropdownMenuItem
+          className="flex items-center justify-center border border-border hover:bg-accent hover:!text-blue-300"
+          onClick={() => {
+            setSelectedFolder(folder);
+            setActiveDialog(DialogOption.rename);
+            setIsDialogOpen(true);
+          }}
+        >
+          Rename
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          className="flex items-center justify-center  border border-border hover:bg-accent hover:!text-blue-300"
+          onClick={() => {
+            setSelectedFolder(folder);
+            setActiveDialog(DialogOption.delete);
+            setIsDialogOpen(true);
+          }}
+        >
+          <Trash2 className="mr-2 h-4 w-4" />
+          Delete
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
