075bc76df1fa205199b70c23033a7f85e1da8265
Andrei Kondrashov <kondraschov.andrew1999@gmail.com>
2025-04-25 20:04:00 -0400

    some refactoring of affected components

--- components/landing-header.tsx
+++ components/landing-header.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import CreateMenu from "@/components/create-menu";
+import CreateDropdown from "@/components/create-menu";
 import { createClient } from "../utils/supabase/server";
 import { signOutAction } from "@/app/actions";
 import { redirect } from "next/navigation";
@@ -16,7 +16,7 @@ export async function LandingHeader({ isProtectedPage = false}: { isProtectedPag
     <header className="w-full flex justify-end p-4 sticky top-0 z-40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 border-b">
       <div className="flex gap-4">
         {/* Link to login page if user is not already signed in, if not dropdown menu for creating study materials */}
-        <CreateMenu isLoggedIn={!!user} />
+        <CreateDropdown isLoggedIn={!!user} />
 
         { isProtectedPage || user ? (
           <form action={signOutAction}>
