ba8de518706eaa699951c92eaab66dfddf1c787b
Andrei Kondrashov <kondraschov.andrew1999@gmail.com>
2025-04-29 12:13:47 -0400

    add action to fetch user folders

--- app/actions.ts
+++ app/actions.ts
@@ -9,9 +9,7 @@ import { PdfToTextConverter } from "@/utils/file-converters/pdf-converter";
 import { DocsxToTextConverter } from "@/utils/file-converters/docx-converter";
 import { PptxToTextConverter } from "@/utils/file-converters/pptx-converter";
 import { generateStudyGuide } from "@/utils/ai/generate-study-guide";
-import generateFlashcards, {
-  createFlashcardSet,
-} from "@/utils/ai/generate-flashcards";
+import { createFlashcardSet } from "@/utils/ai/generate-flashcards";
 import storeFile from "@/utils/file/store-file";
 import { v4 as uuidv4 } from "uuid";
 import deleteFromStorage from "@/utils/file/delete-from-storage";
@@ -582,4 +580,63 @@ export async function deleteFolder(folder_id: string): Promise<{
     success: true,
     message: "Folder was successfully deleted",
   };
+}
+
+export async function fetchUserFolders() {
+  try {
+    const supabase = await createClient();
+    const {
+      data: { user },
+      error: user_err,
+    } = await supabase.auth.getUser();
+
+    if (user_err) {
+      throw new Error("Error fetching user: ", user_err);
+    }
+    if (!user || user === null) throw new Error("User is not authenticated");
+
+    const { data: my_folders_id, error: err } = await supabase.rpc(
+      "get_folder_id_under_root",
+      {
+        target_folder_name: "My Folders",
+      }
+    );
+
+    if (err) {
+      throw new Error("Error fetching My Folders id: ", err);
+    }
+
+    const { data, error } = await supabase
+      .from("folder_in_folder")
+      .select(
+        `
+      id: folder_id,
+      name: folder_name,
+      user (
+        user_id: id
+      )
+    `
+      )
+      .eq("user.auth_user_id", user.id)
+      .eq("parent_id", my_folders_id);
+
+    if (error) {
+      throw new Error("Error fetching user's folders: ", error);
+    }
+    const folders: { id: string; name: string }[] = data.map(
+      ({ user, ...rest }) => rest
+    );
+    return {
+      success: true,
+      message: "Success",
+      content: folders,
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      success: false,
+      message: error instanceof Error ? error.message : "Unknown error",
+      content: [],
+    };
+  }
 }
