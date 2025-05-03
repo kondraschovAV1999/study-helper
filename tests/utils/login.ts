223aab84f307d32308c2edfaea8a857f72f802c7
Andrei Kondrashov <kondraschov.andrew1999@gmail.com>
2025-05-03 15:10:08 -0400

    helper function to login

---
+++ tests/utils/login.ts
@@ -0,0 +1,16 @@
+import { Page, expect, test } from "@playwright/test";
+import { User } from "./seeds";
+
+export async function login(page: Page, user: User) {
+  await test.step("Fill login form", async () => {
+    await page.goto("/login");
+    await page.getByPlaceholder("Enter your email address").fill(user.email);
+    await page.getByPlaceholder("Enter your password").fill(user.password);
+    await page.getByRole("button", { name: "Sign In" }).nth(1).click();
+  });
+
+  await test.step("Verify successful login", async () => {
+    await expect(page).toHaveURL(/protected/);
+    await expect(page.getByText("My Library")).toBeVisible();
+  });
+}
